ccc15587afc1fcabbd3e60bc44c979793b179830
michael-pitt <m_u_pitt@yahoo.com>
2021-11-10 10:31:46 +0100

    update DiSimCode

--- PhysicsTools/NanoAOD/python/protons_cff.py
+++ PhysicsTools/NanoAOD/python/protons_cff.py
@@ -7,59 +7,8 @@ from Validation.CTPPS.simu_config.base_cff import *
 
 # proton reco settings
 #from Validation.CTPPS.simu_config.year_2017_preTS2_cff import ctppsDirectProtonSimulation as ppsDirectSim2017_preTS2
-#from Validation.CTPPS.simu_config.year_2017_postTS2_cff import ctppsDirectProtonSimulation as ppsDirectSim2017_postTS2
+from Validation.CTPPS.simu_config.year_2017_postTS2_cff import ctppsDirectProtonSimulation as ppsDirectSim2017_postTS2
 #from Validation.CTPPS.simu_config.year_2018_cff import ctppsDirectProtonSimulation as ppsDirectSim2018
-#from Validation.CTPPS.simu_config.year_2017_cff import reco_local as reco_local_2017
-#from Validation.CTPPS.simu_config.year_2018_cff import reco_local as reco_local_2018
-
-def SetAperture(ctppsDirectProtonSimulation, year):
-  if year=='2016_preTS2':
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_int = 0.111
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_slp = 0.0
-    ctppsDirectProtonSimulation.empiricalAperture45_a_int = 127.0
-    ctppsDirectProtonSimulation.empiricalAperture45_a_slp = -0.000
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_int = 0.138
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_slp = 0.0
-    ctppsDirectProtonSimulation.empiricalAperture56_a_int = 191.6
-    ctppsDirectProtonSimulation.empiricalAperture56_a_slp = 0.0
-  elif year=='2016_preTS2':
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_int = 0.104
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_slp = 0.0
-    ctppsDirectProtonSimulation.empiricalAperture45_a_int = 116.4
-    ctppsDirectProtonSimulation.empiricalAperture45_a_slp = 0.0
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_int = 0.110
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_slp = 0.0
-    ctppsDirectProtonSimulation.empiricalAperture56_a_int = 150.0
-    ctppsDirectProtonSimulation.empiricalAperture56_a_slp = 0.0
-  elif year=='2017_preTS2':
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_int = 0.066
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_slp = 3.536E-04
-    ctppsDirectProtonSimulation.empiricalAperture45_a_int = 47.7
-    ctppsDirectProtonSimulation.empiricalAperture45_a_slp = 0.447
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_int = 0.062
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_slp = 5.956E-04
-    ctppsDirectProtonSimulation.empiricalAperture56_a_int = -31.9
-    ctppsDirectProtonSimulation.empiricalAperture56_a_slp = 1.323
-  elif year=='2017_postTS2':
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_int = 0.073
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_slp = 4.107E-04
-    ctppsDirectProtonSimulation.empiricalAperture45_a_int = 39.0
-    ctppsDirectProtonSimulation.empiricalAperture45_a_slp = 0.768
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_int = 0.067
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_slp = 6.868E-04
-    ctppsDirectProtonSimulation.empiricalAperture56_a_int = -50.2
-    ctppsDirectProtonSimulation.empiricalAperture56_a_slp = 1.740
-  elif year=='2018':
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_int = 0.079
-    ctppsDirectProtonSimulation.empiricalAperture45_xi0_slp = 4.211E-04
-    ctppsDirectProtonSimulation.empiricalAperture45_a_int = 42.8
-    ctppsDirectProtonSimulation.empiricalAperture45_a_slp = 0.669
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_int = 0.074
-    ctppsDirectProtonSimulation.empiricalAperture56_xi0_slp = 6.604E-04
-    ctppsDirectProtonSimulation.empiricalAperture56_a_int = -22.7
-    ctppsDirectProtonSimulation.empiricalAperture56_a_slp = 1.600
-    
-  return ctppsDirectProtonSimulation
 
 #optics
 from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import *
@@ -152,15 +101,10 @@ ctppsLocalTrackLiteProducer.includePixels = False
 reco_local = cms.Sequence(ctppsLocalTrackLiteProducer)
 
 #setup postTS2 2017 configuration
-
-#ctppsDirectProtonSimulation = ppsDirectSim2017_postTS2
-#reco_local = reco_local_2017
-ctppsDirectProtonSimulation.useEmpiricalApertures = True
-ctppsDirectProtonSimulation=SetAperture(ctppsDirectProtonSimulation,'2017_postTS2')
+ctppsDirectProtonSimulation = ppsDirectSim2017_postTS2.clone()
 ctppsLocalTrackLiteProducer.includeStrips = True
 ctppsLocalTrackLiteProducer.includePixels = True 
 reco_local.insert(0,totemRPUVPatternFinder*totemRPLocalTrackFitter*ctppsPixelLocalTracks)
-
 ctppsOpticalFunctionsESSource.configuration.append(config_2017)
 ctppsRPAlignmentCorrectionsDataESSourceXML.MisalignedFiles = [alignmentFile_2017_postTS2]
 ctppsRPAlignmentCorrectionsDataESSourceXML.RealFiles = [alignmentFile_2017_postTS2]
@@ -199,6 +143,7 @@ protonTablesMC = cms.Sequence(
 )
 
 if singleRPProtons: protonTables.insert(protonTables.index(multiRPTable),singleRPTable)
+if singleRPProtons: protonTablesMC.insert(protonTablesMC.index(multiRPTable),singleRPTable)
 
 (run2_nanoAOD_92X | run2_miniAOD_80XLegacy | run2_nanoAOD_94X2016 | run2_nanoAOD_94X2016 | \
     run2_nanoAOD_94XMiniAODv1 | run2_nanoAOD_94XMiniAODv2 | \
